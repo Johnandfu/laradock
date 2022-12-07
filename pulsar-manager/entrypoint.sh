b93158117ae9992fbefa841430e235af5d0ae7a0
john <john@archlinux.com>
2022-12-07 21:40:24 +0800

    add mirrors

--- pulsar-manager/entrypoint.sh
+++ pulsar-manager/entrypoint.sh
@@ -1,5 +1,4 @@
-#!/bin/bash -x
-#./pulsar-manager/bin/pulsar-manager
-# CSRF_TOKEN=$(curl http://localhost:7750/pulsar-manager/csrf-token)
-# curl -H 'X-XSRF-TOKEN: $CSRF_TOKEN' -H 'Cookie: XSRF-TOKEN=$CSRF_TOKEN;' -H "Content-Type: application/json"  -X PUT http://localhost:7750/pulsar-manager/users/superuser -d '{"name": "admin", "password": "apachepulsar", "description": "test", "email": "username@test.org"}'
-/opt/pulsar-manager/pulsar-manager/bin/pulsar-manager
+#!/usr/bin/env bash
+#/opt/pulsar-manager/pulsar-manager/bin/pulsar-manager
+tail -f /dev/null
+#nohup /opt/pulsar-manager/pulsar-manager/bin/pulsar-manager >> /opt/pulsar-manager/pulsar-manager/bin/pulsar-manager.log 2>&1 &
